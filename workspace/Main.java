1b09e3f7fa2f772eda8551ac08e7ee435021e041
alvyn2 <87878122+alvyn2@users.noreply.github.com>
2025-05-02 14:14:09 +0000

    answered questions

--- workspace/Main.java
+++ workspace/Main.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
     //deleted first row from csv
         //id,Low Temperature,High Temperature,Rain Inches,Snow Inches,Cloud Percent
     
@@ -81,6 +84,21 @@ System.out.println("also no "+network.predict(41,58,49));
 System.out.println("Should give yes "+network.predict(52,71,32));
 System.out.println("Should give something random and different"+network.predict((int)(Math.random()*80), (int)(Math.random()*100),(int)(Math.random()*100)));
 
+
+
+// questions and answers
+/*
+ * 
+ * 
+ *  1) Do these predictions seem reasonable to you? Why or why not? 
+ * No because both quesses are often close to the same number as each other. 
+
+                 2) What are some factors not accounted for by your data which may be affecting your outcomes which your model isn't accounting for?
+                 The dataset includes data from various locatios around the us, but this could make the predictions less accurate because it is adapting to different locations throuhgought the dataset that trains it. It also might not account for the season as well as if it included the day of the year
+                 3)  Given what you now know about neural networks and how they arrive at their predictions, what may be a specific question or kind of question which you should not rely on chatGPT to answer                                         accurately? Explain why. 
+                        You shouldn't rely of chatGPT to answer a question that has answers not following a consistent pattern, such as creative art or location-specific questions
+ * 
+ */
 
     }
 }
